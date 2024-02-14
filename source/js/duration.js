cb1f66cc569e97292b4d8390ff6f9a1193b57b10
mobeicanyue <81098819+mobeicanyue@users.noreply.github.com>
2024-02-14 15:41:31 +0800

    Update footer script and background img and path link

--- source/js/duration.js
+++ source/js/duration.js
@@ -1,18 +1,16 @@
-var now = new Date(); // 获取当前时间
-const copyrightYearElement = document.getElementById("copyrightYear");
+const now = new Date(); // 获取当前时间
+const init = new Date("2023-12-26T22:19:15"); // 设置初始化时间
 
 const currentYear = now.getFullYear(); // 获取当前年份
-copyrightYearElement.textContent = ` - ${currentYear}`;
+
+// 计算运行时间
+const days = Math.floor((now - init) / 1000 / 60 / 60 / 24); // 计算天数
+const years = Math.floor(days / 365); // 计算年数
+const remainingDays = days % 365; // 计算剩余天数
+const hours = Math.floor((now - init) / 1000 / 60 / 60 - 24 * days);
+
 function createTime() {
-    const now = new Date();  // 获取当前时间
-    const init = new Date("12/26/2023 22:19:15");  // 设置建站时间，格式：月/日/年 时:分:秒
-
-    // 计算运行时间
-    const days = Math.floor((now - init) / 1000 / 60 / 60 / 24); // 计算天数
-    const years = Math.floor(days / 365); // 计算年数
-    const remainingDays = days % 365; // 计算剩余天数
-    const hours = Math.floor((now - init) / 1000 / 60 / 60 - 24 * days);
-
+    document.getElementById("copyrightYear").textContent = ` - ${currentYear}`;
     document.getElementById("timeYear").textContent = `本站已运行 ${years} 年`;
     document.getElementById("timeDate").textContent = ` ${remainingDays} 天`;
     document.getElementById("times").textContent = ` ${hours < 10 ? "0" : ""}${hours} 小时`;
